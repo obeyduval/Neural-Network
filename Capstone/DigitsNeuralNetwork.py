90b9e1ecd6a25dd3e3fa322b9ce8d28a4d382337
obeyduval <C22Timothy.Jackson@usafa.edu>
2021-08-20 10:28:47 -0600

    1.5
    Neural Net Starter

--- Capstone/DigitsNeuralNetwork.py
+++ Capstone/DigitsNeuralNetwork.py
@@ -1,1 +1,4 @@
-#Capstone Digits Neural Network Artificat
+# Capstone Digits Neural Network Artifact
+# C1C Timothy Jackson, C1C Jake Lee, C1C Christopher Meixsell
+# Digits Neural Network
+# Novotny Article (Machine Learning)
