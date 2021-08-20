d3d17b277de040c76cc20bdf8e4b2ef26437ad44
obeyduval <C22Timothy.Jackson@usafa.edu>
2021-08-20 10:39:18 -0600

    2.0
    Perceptron good

--- perceptron.py
+++ perceptron.py
@@ -1,18 +1,21 @@
 import numpy as np
-#from https://towardsdatascience.com/machine-learning-for-beginners-an-introduction-to-neural-networks-d49f22d238f9 as a guide
+
+
 def sigmoid(x):
-  # Our activation function: f(x) = 1 / (1 + e^(-x))
-  return 1 / (1 + np.exp(-x))
+    # Our activation function: f(x) = 1 / (1 + e^(-x))
+    return 1 / (1 + np.exp(-x))
+
 
 class Perceptron:
-  def __init__(self, weights, bias):
-    self.weights = weights
-    self.bias = bias
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
 
-  def feedforward(self, inputs):
-    # Weight inputs, add bias, then use the activation function
-    total = np.dot(self.weights, inputs) + self.bias
-    return sigmoid(total)
+    def feedforward(self, inputs):
+        # Weight inputs, add bias, then use the activation function
+        total = np.dot(self.weights, inputs) + self.bias
+        return sigmoid(total)
+
 
 weights = np.array([0, 1])
 bias = 4
